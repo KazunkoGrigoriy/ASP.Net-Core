1b26c2d81be6bef3eb78a2911eef9427d6cc53ad
Grigoriy Kazunko <gkazunko@mail.ru>
2022-03-02 23:22:29 +0300

    Домашняя работа №21

---
+++ HomeWork20/File.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWork20
+{
+    public class File
+    {
+        public List<Record> Getinfo()
+        {
+            string json = System.IO.File.ReadAllText("records.json");
+            return JsonConvert.DeserializeObject<List<Record>>(json);
+        }
+    }
+}
